de89de6f4a339a74a5c3aeb2132267a97a832527
zkaj2g5@gmail.com <zkaj2g5@gmail.com>
2021-01-12 20:35:09 -0500

    Heroku Stuff

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const { KEY, USER } = process.env;
 const DB_NAME = 'linkerator-db';
 const DB_URL =
   process.env.DATABASE_URL ||
-  `postgres://${USER}:${KEY}@localhost:5432/${DB_NAME}?ssl=true`;
+  `postgres://${USER}:${KEY}@localhost:5432/${DB_NAME}`;
 
 // Create the client
 const client = new Client(DB_URL);
