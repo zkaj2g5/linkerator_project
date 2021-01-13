d04575be1bed8be61df567071c08fb7b66fddc55
zkaj2g5@gmail.com <zkaj2g5@gmail.com>
2021-01-12 19:51:51 -0500

    .

---
+++ build/precache-manifest.78eaac5dcb87ae054d68e152be0f7e02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af5c02227f7ecfe7d5c68cb1fc6666b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e59af23b35c6ee55b556",
+    "url": "/static/css/2.842e1da4.chunk.css"
+  },
+  {
+    "revision": "56a5826ac1660ba48776",
+    "url": "/static/css/main.b2b7b8fb.chunk.css"
+  },
+  {
+    "revision": "e59af23b35c6ee55b556",
+    "url": "/static/js/2.9e326e9c.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.9e326e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a5826ac1660ba48776",
+    "url": "/static/js/main.fc9fe02f.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
